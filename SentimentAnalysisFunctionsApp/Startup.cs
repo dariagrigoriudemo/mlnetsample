1222c064007f5cd437b77366b288b4ba9adc7178
Daria Grigoriu <dariac@ntdev.microsoft.com>
2019-08-07 14:22:02 -0700

    Add project files.

---
+++ SentimentAnalysisFunctionsApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.ML;
+using SentimentAnalysisFunctionsApp;
+using SentimentAnalysisFunctionsApp.DataModels;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace SentimentAnalysisFunctionsApp
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddPredictionEnginePool<SentimentData, SentimentPrediction>()
+                .FromFile("MLModels/sentiment_model.zip");
+        }
+    }
+}
